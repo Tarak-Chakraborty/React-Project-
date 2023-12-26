2dd5250ace0e90019eb4bcc32f8255367d1d76ee
Tarak Chakraborty <Chakrabortytarak83@gmail.com>
2023-12-26 18:42:39 +0530

    changes in logo component

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -14,17 +14,17 @@ function Logo({ width = '100px' }) {
   return (
     <div style={{ 
       fontFamily: 'Arial, sans-serif', 
-      fontSize: '2rem', // Increased font size for emphasis
+      fontSize: '2rem', 
       fontWeight: 'bold',
-      color: '#48BB78', // Light green color, adjust as per preference
-      textShadow: '2px 2px 2px rgba(0, 0, 0, 0.2)', // Adds a subtle text shadow
-      width: width, // You can customize the width through the 'width' prop
-      textAlign: 'center', // Center-align the text
-      lineHeight: '1', // Adjusts line height for better spacing
-      letterSpacing: '2px', // Increases letter spacing for clarity
-      border: '1px solid #48BB78', // Adds a border around the text
-      borderRadius: '8px', // Rounds the corners of the border
-      padding: '10px' // Adds padding to the text for breathing room
+      color: '#48BB78',
+      textShadow: '2px 2px 2px rgba(0, 0, 0, 0.2)', 
+      width: width, 
+      textAlign: 'center', 
+      lineHeight: '1', 
+      letterSpacing: '2px', 
+      border: '1px solid #48BB78', 
+      borderRadius: '8px', 
+      padding: '10px' 
     }}>
       MegaBlog
     </div>
